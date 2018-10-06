3ffac468d92ff215db8728a6aab90bd582ad808d
Lam Nhan <lamnhan92@gmail.com>
2018-10-06 17:23:56 +0700

    using app-scripts

--- src/example.ts
+++ src/example.ts
@@ -1,14 +1,11 @@
-declare const handlebarsModuleExports: {(): any};
+declare const HandlebarsModule: {()};
+var Handlebars = Handlebars || HandlebarsModule();
 
-export function Handlebars() {
-	return handlebarsModuleExports();
-}
-
-export function sheetbase_handlebars_example1(): void {
+export function example1(): void {
 	const source: string = '<p>Hello, my name is {{name}}. ' +
 							'I am from {{hometown}}. I have {{kids.length}} kids:</p>' +
 							'<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>';
-	const template = Handlebars().compile(source);
+	const template = Handlebars.compile(source);
 	const data = {
 					name: 'Alan',
 					hometown: 'Somewhere, TX',
